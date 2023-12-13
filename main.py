96b3b7e537f9223ed96f402f5e2f3fa80ea0db82
Andrei Kalinichenko <muse2140@gmail.com>
2023-12-13 21:00:08 +0200

    Added

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 print("Hello World")
 print("Go")
+
+hours = int(input("Enter hours: "))
+
+if 12 <= hours < 24:
+    print("PM")
+elif 0 <= hours < 12:
+    print("AM")
+else:
+    print("Incorrect hours!")
+
+
